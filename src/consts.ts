39f8c1d1633ecfc789983d9f2ff8498d42c06e60
kissy <kissyou1015@outlook.jp>
2026-01-16 15:51:45 +0900

    Remove @astrojs/cloudflare adapter for static deployment

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'imkisyou';
+export const SITE_DESCRIPTION = '開発・学習の記録';
