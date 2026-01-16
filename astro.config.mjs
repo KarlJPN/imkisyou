4726d98787d02495f60e7b5857b18187675cefe9
kissy <kissyou1015@outlook.jp>
2026-01-16 15:56:36 +0900

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://karljpn.github.io',
+	base: '/imkisyou',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
